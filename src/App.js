b02f7fe4292ce2c8711fa5febb36f5aebad83343
Alex91328 <57780963+Alex91328@users.noreply.github.com>
2020-03-04 16:42:18 -0700

    Other Attempt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+
+
 import './App.css';
+import PIUComponent from './components/PIUcomponent';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PIUComponent />
+      <Person LastName="Doe" FirstName="Jane" Age={45} HairColor="Black" />
+      <Person LastName="Smith" FirstName="John" Age={88} HairColor="Brown" />
+      <Person LastName="Smith" FirstName="Maria" Age={55} HairColor="Brown" />
+      <Person LastName="Fillmore" FirstName="Millard" Age={50} HairColor="Brown" />
     </div>
   );
 }
---
+++ src/components/PIUcomponent.js
@@ -0,0 +1,9 @@
+import React, { component, Component } from 'react';
+
+class PIUComponent extends Component {
+    render() {
+        return (<div></div>)
+    }
+}
+
+export default PIUComponent;
