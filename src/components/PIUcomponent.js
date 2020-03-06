0074d9c67adc35ffb219b6cdc51a1660fe293bab
Alex91328 <57780963+Alex91328@users.noreply.github.com>
2020-03-05 17:28:46 -0700

    Third Commit

--- src/components/PIUcomponent.js
+++ src/components/PIUcomponent.js
@@ -1,4 +1,4 @@
-import React, { component, Component } from 'react';
+import React, { Component } from 'react';
 
 class PIUComponent extends Component {
     constructor(props) {
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class personCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ageCount: props.Age
-        }
-    }
-    handleClick = () => {
-        this.setState({
-            ageCount: this.state.ageCount + 1
+const PersonCard = props => {
+
+
+    const [state, setState] = useState({
+        ageCount: props.Age
+    });
+
+
+    const handleClick = () => {
+        setState({
+            ageCount: state.ageCount + 1
         })
-    }
-    render() {
-        const { FirstName, LastName, HairColor } = this.props;
-        return (
-            <div>
-                <h1>{LastName}, {FirstName}</h1>
-                <h6>Age:{this.state.ageCount}</h6>
-                <h6>Hair Color: {HairColor}</h6>
-                <button onClick={this.handleClick}>Birthday For {FirstName} {LastName}</button>
-            </div>
-        );
-    }
+    };
+
+    const { FirstName, LastName, HairColor } = props
+    return (
+        <div>
+            <h1>{LastName}, {FirstName}</h1>
+            <h6>Age:{state.ageCount}</h6>
+            <h6>Hair Color: {HairColor}</h6>
+            <button onClick={handleClick}>Birthday For {FirstName} {LastName}</button>
+        </div>
+    );
 }
-export default personCard;
+
+export default PersonCard;
